056803d0bb697d1bea544a44a9a1ca75ab371935
lishengyu <lishengyu@gzlplink.com>
2022-05-16 09:15:21 +0800

    修改为协程，提高性能

--- spider/umei.py
+++ spider/umei.py
@@ -1,15 +1,10 @@
-import requests
+import aiohttp
+import asyncio
+
 from bs4 import BeautifulSoup
-import time
 import os
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-
-#关闭安全请求警告
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-
-dir_path = "/pic"
+dir_path = "pic"
 main_page = 'https://www.umei.cc'
 url = 'https://www.umei.cc/e/action/get_img_a.php'
 
@@ -20,47 +15,60 @@ headers = {
 if not os.path.isdir(dir_path):
     os.mkdir(dir_path)
 
-num = 1
-while 1:
-    params_dict = {
-        "next": num,
-        "table": "news",
-        "action": "getmorenews",
-        "limit": 10,
-        "small_length": 120,
-        "classid": 25
-    }
-    try:
+all_pic_url = []
+async def get_url(params_dict):
+    async with aiohttp.ClientSession() as seccion:
+        async with seccion.post(url,headers=headers,data=params_dict) as resp:
+            pic_obj = BeautifulSoup(await resp.text(), 'html.parser')  #需要加个await
+            ret = pic_obj.find_all('a')
+            for uri in ret:
+                # 获取uri以及图片的名称
+                child_url = main_page + uri.get('href')
+                child_name = uri.text
 
-        ret = requests.post(url, headers=headers, data=params_dict, verify=False)
-        if ret.status_code != 200:
-            continue
+                # 请求图片详情页
+                async with seccion.get(child_url, headers=headers) as child_ret:
+                    child_ret.encoding = 'utf-8'
 
-        if "img src=" not in ret.text:
-            break
-        pic_obj = BeautifulSoup(ret.text, 'html.parser')
-        ret = pic_obj.find_all('a')
-        for uri in ret:
-            # 获取uri以及图片的名称
-            child_url = main_page + uri.get('href')
-            child_name = uri.text
+                    child_obj = BeautifulSoup(await child_ret.text(), 'html.parser')
+                    # 获取最终图片地址
+                    pic = child_obj.find('section', attrs={"class": "img-content"}).find('img').get('src')
 
-            # 请求图片详情页
-            child_ret = requests.get(child_url, headers=headers, verify=False)
-            child_ret.encoding = 'utf-8'
+                    all_pic_url.append(pic)
 
-            child_obj = BeautifulSoup(child_ret.text, 'html.parser')
-            # 获取最终图片地址
-            pic = child_obj.find('section', attrs={"class": "img-content"}).find('img').get('src')
+async def download_pic(url):
+    async with aiohttp.ClientSession() as seccion:
+        async with seccion.get(url,headers=headers) as get_resp:
+            pic_name = url.rsplit('/',1)[-1]
+            with open(os.path.join(dir_path,pic_name),mode='wb') as fw:
+                fw.write(await get_resp.content.read())
+                print(f'download {pic_name} done....')
 
-            pic_name = pic.split('/')[-1]
 
-            # 获取图片
-            pic_ret = requests.get(pic, headers=headers, verify=False)
-            with open(os.path.join(dir_path, pic_name), mode='wb') as fw:
-                fw.write(pic_ret.content)
-                print(pic_name, ' ok')
-    except:
-        pass
-    time.sleep(1)
-    num += 1
+async def main():
+    tasks = []
+    for num in range(100):
+        params_dict = {
+            "next": num,
+            "table": "news",
+            "action": "getmorenews",
+            "limit": 10,
+            "small_length": 120,
+            "classid": 25
+        }
+        tasks.append(get_url(params_dict))
+    await asyncio.wait(tasks)
+
+
+
+async def aiodown_main():
+    tasks = []
+    for url in all_pic_url:
+        tasks.append(download_pic(url))
+    await asyncio.wait(tasks)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
+    print('url downLoad ok。。。')
+    asyncio.get_event_loop().run_until_complete(aiodown_main())
